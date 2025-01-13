dc61d24ec1d4261a04f59f0286fcf5598bccd463
Zhunx <zhunxbusiness@gmail.com>
2025-01-13 19:35:22 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game       'gta5'
+lua54      'yes'
+
+Author     'OrionNetworkStore'
+           'AlejoSPlaza'
+           
+description 'Script Anti Bunny Jump'
+
+client_scripts {
+    'client/client.lua'
+}
